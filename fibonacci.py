8a75f25e5706c476130acbbf644b7cc01a909773
Tongyi Dou <47798921+asdstory@users.noreply.github.com>
2020-04-22 10:50:27 -0400

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,41 @@
+# fibonacci 1
+
+def fab(n):
+    n1 = 1
+    n2 = 1
+    n3 = 1
+
+    if n < 1:
+        print('Wrong Input! ')
+        return -1
+
+    while (n-2) > 0:
+        n3 = n2 + n1
+        n1 = n2
+        n2 = n3
+        n -= 1
+    
+    return n3
+
+number = int(input('Please input an integer: '))
+result = fab(number)
+if result != -1:
+    print('There are totally %d rabbit born! ' % result)
+
+
+# fibonacci 1
+
+def fab(n):
+    if n < 1:
+        print('Wrong Input! ')
+        return -1
+
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return fab(n-1) + fab(n-2)
+
+number = int(input('Please input an integer: '))
+result = fab(number)
+if result != -1:
+    print('There are totally %d rabbit born! ' % result)
