23a1c09b3fc2e956825db3b6eeaa82abfb72d0df
Tongyi Dou <47798921+asdstory@users.noreply.github.com>
2020-04-21 22:47:03 -0400

    Create wordgame_1.py

---
+++ wordgame_1.py
@@ -0,0 +1,10 @@
+print('--------------This is a word game #1--------------')
+temp = input("Guess the number I have in my mind: ")
+guess = int(temp)
+if guess == 8:
+    print("Great?!")
+    print("Anyway, there is no bonus!")
+else:
+    print("No, it is 8!")
+print("Game Over^_^")
+
